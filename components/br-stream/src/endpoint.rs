ba3a1b32fea611f54d8f2e6bcd5f4f27a3ae6232
Yu Juncen <yujuncen@pingcap.com>
2022-02-24 15:20:14 +0800

    more debug info
    Signed-off-by: Yu Juncen <yujuncen@pingcap.com>

--- components/br-stream/src/endpoint.rs
+++ components/br-stream/src/endpoint.rs
@@ -380,6 +380,7 @@ where
             //       Or if there are too many duplicated `Flush` command, we may do some useless works.
             let new_rts = Self::try_resolve(pd_cli.clone(), resolvers).await;
             if let Some(rts) = router.do_flush(&task, store_id, new_rts).await {
+                info!("flushing and refreshing checkpoint ts."; "checkpoint_ts" => %rts);
                 if rts == 0 {
                     // We cannot advance the resolved ts for now.
                     return;
